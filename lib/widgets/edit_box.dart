d6cc4f48c7a8070e98d3f757696f970a1a9ca603
Vrushti <vrushtishah24@gmail.com>
2023-08-31 15:32:03 +0530

    fixed bugs and changed UI

---
+++ lib/widgets/edit_box.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controller/timeController.dart';
+import '../modal/timeValue.dart';
+
+editBox(
+  TimeValue timeValue,
+  TextEditingController textController,
+) {
+  return TextField(
+    controller: textController,
+    onChanged: (value) {
+      int seconds = int.tryParse(value) ?? 0;
+      timeValue.currentSeconds.value = seconds;
+    },
+    decoration: InputDecoration(
+      focusedBorder: OutlineInputBorder(
+        borderSide: const BorderSide(width: 3, color: Colors.black87),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      labelText: 'sec',
+      labelStyle: TextStyle(color: Colors.black87),
+      enabledBorder: OutlineInputBorder(
+        borderSide: const BorderSide(width: 2.5, color: Colors.black87),
+        borderRadius: BorderRadius.circular(10),
+      ),
+    ),
+    keyboardType: TextInputType.number,
+    style: TextStyle(color: Colors.black87, fontSize: 18),
+  );
+}
--- lib/widgets/enter_timer.dart
+++ lib/widgets/enter_timer.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../controller/timeController.dart';
 import '../main.dart';
 import '../modal/timeValue.dart';
+import 'edit_box.dart';
 
 enterTimer(TimeValue timeValue, TextEditingController textController,
     RxBool isTimerOn) {
@@ -14,28 +16,69 @@ enterTimer(TimeValue timeValue, TextEditingController textController,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          TextFormField(
-            controller: textController,
-            enabled: !isTimerOn.value,
-            onChanged: ((value) {
-              int seconds = int.parse(value);
-              timeValue.currentSeconds.value = seconds;
-            }),
-            decoration: InputDecoration(
-                focusedBorder: OutlineInputBorder(
+          Obx(() {
+            final bool isTimerRunning = timeValue.isRunning.value;
+            final bool isEditingEnabled =
+                (!isTimerRunning && timeValue.initialSeconds == 0) ||
+                    (!isTimerRunning && !timeValue.isTimerOn.value) ||
+                    (!isTimerRunning && timeValue.initialSeconds != 0);
+
+            if (isEditingEnabled) {
+              return TextFormField(
+                controller: textController,
+                onChanged: (value) {
+                  if (!isTimerRunning) {
+                    if (value.isEmpty) {
+                      timeValue.currentSeconds.value = 0;
+                    } else {
+                      int seconds = int.tryParse(value) ?? 0;
+                      timeValue.currentSeconds.value = seconds;
+                      timeValue.initialSeconds = seconds;
+                    }
+                  }
+                },
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
                     borderSide:
                         const BorderSide(width: 3, color: Colors.black87),
-                    borderRadius: BorderRadius.circular(10)),
-                labelText: 'sec',
-                labelStyle: TextStyle(color: Colors.black87),
-                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  labelText: 'sec',
+                  labelStyle: TextStyle(color: Colors.black87),
+                  enabledBorder: OutlineInputBorder(
                     borderSide:
                         const BorderSide(width: 2.5, color: Colors.black87),
-                    borderRadius: BorderRadius.circular(10))),
-            keyboardType: TextInputType.number,
-            readOnly: isTimerOn.value,
-            style: TextStyle(color: Colors.black87, fontSize: 18),
-          ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                readOnly: isTimerRunning || !isEditingEnabled,
+                style: TextStyle(color: Colors.black87, fontSize: 18),
+              );
+            } else if (timeValue.currentSeconds.value == 0) {
+              textController.text = '';
+              return editBox(timeValue, textController);
+            } else {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  width: mq.width * .2,
+                  height: mq.height * .075,
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 144, 140, 140),
+                      border: Border.all(width: 2),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10))),
+                  child: Center(
+                    child: Text(
+                      '${timeValue.initialSeconds}',
+                      style: TextStyle(color: Colors.black26),
+                    ),
+                  ),
+                ),
+              );
+            }
+          }),
         ],
       ),
     ),
